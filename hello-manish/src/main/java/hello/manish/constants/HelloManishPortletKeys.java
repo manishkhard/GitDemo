271dc0b86416507ea32ecdc0a09b435e3df51e0f
manishkhard <guptamanish2110gmail.com>
2020-07-27 17:26:16 +0530

    commit for jenkins job

---
+++ hello-manish/src/main/java/hello/manish/constants/HelloManishPortletKeys.java
@@ -0,0 +1,11 @@
+package hello.manish.constants;
+
+/**
+ * @author rishb
+ */
+public class HelloManishPortletKeys {
+
+	public static final String HELLOMANISH =
+		"hello_manish_HelloManishPortlet";
+
+}
